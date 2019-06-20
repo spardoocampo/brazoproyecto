06ab9089d93cc11419a8656f8e9147ff176d2522
spardoocampo <sebastian.pardoo@uqvirtual.edu.co>
2019-06-20 16:55:23 -0500

    ciclo for

--- brazoproyecto.ino
+++ brazoproyecto.ino
@@ -3,60 +3,54 @@
 #define motorderecha 11
 #define motorizquierda 10
 #define motormano 9
-Servo Base;//Declaro el servomotor de la base
+Servo base;//Declaro el servomotor de la base
+Servo derecha;
+Servo izquierda;
+Servo mano;
 int angulo = 0;
 
 void setup() {
   // put your setup code here, to run once:
- Base.attach(motorBase);//inicializamos el servo
-
- for(angulo = 0;angulo <= 180;angulo++){
- Base.write(angulo);
+ base.attach(motorBase);//inicializamos el servo
+ for(angulo=0;angulo<=180;angulo++)
+ {base.write(angulo);
+ delay(50);
+ }
+ for(angulo=180;angulo>=90;angulo--)
+ {base.write(angulo);
+ delay(50);
+ }
+ 
+ derecha.attach(motorderecha);
+ for(angulo=47;angulo<=116;angulo++)
+ {derecha.write(angulo);
  delay(100);
  }
- for(angulo = 180;angulo >= 0;angulo--){
- Base.write(angulo);
- delay(100);
+ for(angulo=116;angulo>=81;angulo--)//max116min47medio81
+ {derecha.write(angulo);
+ delay(100);//el motor el mimnimo de grados es 47
  }
  
- Base.attach(motorderecha);//el motor el mimnimo de grados es 47
- Base.write(47);
- delay(2000);
- Base.write(90);
- delay(2000);
- Base.write(116);//max116
- delay(2000);
- Base.write(90);
- delay(2000);
- 
- Base.attach(motorizquierda);//inicializamos el servo
- Base.write(90);//min85
- delay(2000);
- Base.write(120);
- delay(2000);
- Base.write(160);
- delay(2000);
- Base.write(85);
- delay(2000);
- 
-Base.attach(motormano);//inicializamos el servo
- Base.write(100);//min100
- delay(2000);
- Base.write(110);
- delay(2000);
- Base.write(115);//max140
- delay(2000);
- Base.write(120);
- delay(2000);
-  Base.write(125);//min100
- delay(2000);
- Base.write(130);
- delay(2000);
- Base.write(135);//max140
- delay(2000);
- Base.write(140);
- delay(2000);
+ izquierda.attach(motorizquierda);//inicializamos el servo
+ for(angulo=85;angulo<=160;angulo++)
+ {izquierda.write(angulo);
+ delay(100);
+ }
+ for(angulo=160;angulo>=112;angulo--)//max160min85medio112
+ {izquierda.write(angulo);
+ delay(100);//el motor el mimnimo de grados es 47
+ }
 
+ 
+mano.attach(motormano);//inicializamos el servo
+ for(angulo=100;angulo<=140;angulo++)
+ {mano.write(angulo);
+ delay(100);
+ }
+ for(angulo=140;angulo>=120;angulo--)//max140min100medio120
+ {mano.write(angulo);
+ delay(100);//el motor el mimnimo de grados es 47
+ }
 
 }
 void loop() {
